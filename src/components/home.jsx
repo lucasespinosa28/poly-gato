fcb38dda5a41e94051ed28fd1d60f7108bf91209
lucasespinosa28 <52639395+lucasespinosa28@users.noreply.github.com>
2022-02-08 21:25:38 -0300

    fix svg path

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -12,7 +12,7 @@ function Home(){
             <Link className='text-2xl font-bold m-4 font-bold underline hover:text-lime-400 my-2' to="/combine">go to Combine</Link>
             <div className='flex  justify-center'>
             <img className="max-w-[27em]" src="./0.png" alt="cat" />
-                <img className="max-w-[5em]" src=".plus.svg" alt="green plus symbol" />
+                <img className="max-w-[5em]" src="./plus.svg" alt="green plus symbol" />
                 <img className="max-w-[27em]" src="./54.png" alt="hood" />
                 <img className="max-w-[5em]" src="./equal.svg" alt="green equal symbol" />
                 <img className="max-w-[27em]" src="./5400.png" alt="cat with hood" />
